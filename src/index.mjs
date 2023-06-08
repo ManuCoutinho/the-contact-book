3657db2231953163f14d6b3dc8222f0cc678bc3a
ManuCoutinho <gdn.sasha@gmail.com>
2023-06-08 13:19:55 -0300

    chore: :package: added cors

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,11 @@
 import express from 'express'
 import { routes } from './routes.mjs'
+import cors from 'cors'
 import 'express-async-errors'
 
 const app = express()
-
+// reminder: for production environment set cors policy
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use((error, request, res, next) => {
